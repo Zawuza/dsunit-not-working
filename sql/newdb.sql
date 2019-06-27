456ff8688f20f55b97d6e092dbfd2cd3779905a7
Andrei Aleksandrov <andrei.aleksandrov@zalando.de>
2019-06-27 14:42:03 +0200

    - Add extended schema

--- sql/newdb.sql
+++ sql/newdb.sql
@@ -8,3 +8,20 @@ CREATE TABLE words(
   language CHAR(2) NOT NULL REFERENCES languages,
   name TEXT UNIQUE NOT NULL
 );
+
+CREATE FUNCTION insert_language_trigger() 
+  RETURNS TRIGGER
+  LANGUAGE plpgsql AS
+  $$
+  BEGIN
+    NEW.code := '11';
+    RETURN NEW;
+  END
+  $$;
+
+CREATE TRIGGER insert_language BEFORE INSERT 
+  ON languages
+  FOR EACH ROW
+  EXECUTE PROCEDURE insert_language_trigger();
+
+INSERT INTO languages(name) VALUES ('Russian');
